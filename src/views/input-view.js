4394f2bab570f2b1e8c1ae71aeb31c0f08de81f6
Marcus Asplund <rootfood@gmail.com>
2017-05-02 11:17:51 +0200

    :star: :tada: update to Hyperapp 0.9.0

--- src/views/input-view.js
+++ src/views/input-view.js
@@ -5,9 +5,9 @@ export const InputView = ({actions, className}) =>
   <div class={className}>
     <input
       type='text'
-      onInput={e => actions.parseRemote(e)}
+      oninput={e => actions.parseRemote(e)}
       placeholder='Klistra in url till fil' />
     <textarea
-      onInput={e => actions.parseString(e)}
+      oninput={e => actions.parseString(e)}
       placeholder='Klistra in tabell med två kolumner med X o Y, N o E eller lat o lng' />
   </div>
