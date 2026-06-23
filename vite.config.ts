d2d24a6977d708698fc7312791cce5caaa9b8b93
Marcus Asplund <rootfood@gmail.com>
2026-06-23 18:09:16 +0200

    Fix asset base path for /sweref deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/sweref/',
   plugins: [solidPlugin(), VitePWA({ registerType: 'autoUpdate' })],
   build: {
     target: 'esnext'
